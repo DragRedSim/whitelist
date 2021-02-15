395cecb06e328a4b77ac7f47f3c2710c629aa216
David Ross Smith <github@dragredsim.cyou>
2021-02-15 23:24:27 +1100

    Update cashrewards.py

--- scripts/cashrewards.py
+++ scripts/cashrewards.py
@@ -299,9 +299,8 @@ if db_exists:
 						sql_add = " INSERT OR IGNORE INTO domainlist (type, domain, enabled, comment) VALUES {} "  .format(nW[sql_index])
 						cursor.executescript(sql_add)
 						sql_get_id = "SELECT id FROM domainlist WHERE domain LIKE '{0}'".format(newWhiteList[a-1])
-						print(sql_get_id)
-						cursor.executescript(sql_get_id)
-						sql_id = cursor.fetchone()
+						sql_get_id = cursor.executescript(sql_get_id)
+						sql_id = sql_get_id.fetchone()
 						print(sql_id)
 						sql_add_category = " INSERT OR IGNORE INTO domainlist_by_group (domainlist_id, group_id) VALUES {}, {} " .format(sql_id, cr_category_id); #TODO fix this to add to a certain group
 						print(sql_add_category)
